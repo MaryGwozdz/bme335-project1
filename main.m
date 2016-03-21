0c8b69c5ea8c82396852bbde78ecbc78578a3324
MaryGwozdz <gwozdzmary@gmail.com>
2016-03-21 12:21:02 -0500

    Update main.m
    Added marginal PDFs and CDFs normalized by total from given pdfs

--- main.m
+++ main.m
@@ -7,6 +7,9 @@ b = linspace(0,1,(1-0)/0.01+1);
 alp = 11.6;
 bet = 5.3;
 
+totT1 = 0;
+totT2 = 0;
+
 pdf_1 = []; %pdf matrix of the first equation
 pdf_2 = []; %pdf matrix of the second equation
 for x = 1:length(a)
@@ -14,8 +17,54 @@ for x = 1:length(a)
         % calculate values of each pdf matrix
         pdf_1(y,x) = alp * ( 0.05 + a(x)^2 ) * ( (b(y)-1)^4 + 0.025 );
         pdf_2(y,x) = bet * ( 1 - a(x)^2 ) * ( 0.05 + b(y)^4 + (a(x)^2) * (b(y)^2) /2 );
+        totT1 = totT1 + pdf_1(y,x);
+        totT2 = totT2 + pdf_2(y,x);
     end
 end
+
+%Create Marginals
+for x = 1:length(a)
+    mPDFaT1(x) = sum(pdf_1(:,x))/totT1; %Marginal PDF for A in T1 cells
+    
+    %Marginal CDF for A in T1 cells
+    if x < 2
+        mCDFaT1(x) = mPDFaT1(x);
+    else
+    mCDFaT1(x) = mCDFaT1(x-1) + mPDFaT1(x);
+    end
+    
+    mPDFaT2(x) = sum(pdf_2(:,x))/totT2; %Marginal PDF for A in T2 cells
+    
+    %Marginal CDF for A in T2 cells
+    if x < 2
+        mCDFaT2(x) = mPDFaT2(x);
+    else
+    mCDFaT2(x) = mCDFaT2(x-1) + mPDFaT2(x);
+    end
+end
+
+for y = 1:length(b)
+    mPDFbT1(y) = sum(pdf_1(y,:))/totT1; %Marginal PDF for B in T1 cells
+    
+    %Marginal CDF for B in T1 cells
+    if y < 2
+        mCDFbT1(y) = mPDFbT1(y);
+    else
+    mCDFbT1(y) = mCDFbT1(y-1) + mPDFbT1(y);
+    end
+    
+    mPDFbT2(y) = sum(pdf_2(y,:))/totT2; %Marginal PDF for B in T2 cells
+   
+    %Marginal CDF for B in T2 cells
+    if y < 2
+        mCDFbT2(y) = mPDFbT2(y);
+    else
+    mCDFbT2(y) = mCDFbT2(y-1) + mPDFbT2(y);
+    end
+    
+end
+
+
 %plot first pdf 
 subplot(1,2,1);
 surf(a,b,pdf_1);
@@ -30,3 +79,37 @@ title('T2 pdf');
 xlabel('a');
 ylabel('b');
 hold on
+
+%plot marginal pdfs
+figure;
+subplot(2,1,1);
+plot(a,mPDFaT1);
+hold on;
+plot(b,mPDFbT1, 'r');
+ylabel('T1');
+title('Marginal PDFs T1 cells');
+legend('a', 'b');
+subplot(2,1,2);
+plot(b,mPDFbT2);
+hold on;
+plot(a,mPDFaT2, 'r');
+ylabel('T2');
+title('Marginal PDFs for T2 cells');
+legend('b', 'a');
+
+%plot marginal CDFs
+figure;
+subplot(2,1,1);
+plot(a,mCDFaT1);
+hold on;
+plot(b,mCDFbT1, 'r');
+ylabel('T1');
+title('Marginal CDFs T1 cells');
+legend('a', 'b');
+subplot(2,1,2);
+plot(b,mCDFbT2);
+hold on;
+plot(a,mCDFaT2, 'r');
+ylabel('T2');
+title('Marginal CDFs for T2 cells');
+legend('b', 'a');
